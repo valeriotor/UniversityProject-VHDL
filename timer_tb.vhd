df4f5a2afd75e96112e8b6530309d793d288370c
Valerio Torsiello <valeriotorsiello@libero.it>
2019-12-31 01:20:39 +0100

    Miglioramento del testbench di timer, con aggiunta di assert.

--- timer_tb.vhd
+++ timer_tb.vhd
@@ -61,35 +61,85 @@ BEGIN
       wait for 100 ns;	
 		reset <= '0';
 		
+		
 		start <= '1';
 		wait for clock_period * 1;
 		start <= '0';
-		wait for clock_period * 1;
-		
 		wait for clock_period * 10;
+		assert (Finished = '1') report "Incorrect output #1: expected 1, received " & std_logic'image(Finished) severity error;
+		wait for clock_period * 1;
+		assert (Finished = '0') report "Incorrect output #2: expected 0, received " & std_logic'image(Finished) severity error;
+		-- ^^ Verifico correttezza dell'impulso di uscita (sia l'istante d'uscita (#1) sia il suo essere impulsivo (#2))
 		
+		
+		wait for 20 ns;
 		reset <= '1';
 		wait for clock_period * 3;
 		reset <= '0';
 		wait for clock_period * 1;
-		start <= '1';
-		wait for clock_period * 5;
-		start <= '0';
 		
+		start <= '1';
+		wait for clock_period * 1;
+		start <= '0';
+		for i in 0 to 9 loop
+			assert (Finished = '0') report "Incorrect output #3: expected 0, received " & std_logic'image(Finished) severity error;
+			wait for clock_period;
+		end loop;	
+		-- ^^ Verifico che il segnale d'uscita non sia generato prima del dovuto
+		
+		
+		wait for 20 ns;
 		reset <= '1';
 		wait for clock_period * 5;
 		reset <= '0';
 		wait for clock_period * 1;
 		
 		start <= '1';
-		wait for clock_period * 10;
+		wait for clock_period * 5;
 		start <= '0';
+		wait for clock_period * 5;
+		assert (Finished = '0') report "Incorrect output #4: expected 0, received " & std_logic'image(Finished) severity error;
+		-- ^^ Verifico che il mantenimento del segnale di start non causi problemi
 		
 		
+		wait for 20 ns;
+		reset <= '1';
+		wait for clock_period * 5;
+		reset <= '0';
+		wait for clock_period * 1;
+		
+		wait for clock_period / 2;
+		start <= '1';
+		wait for clock_period * 1;
+		start <= '0';
+		wait for clock_period * 9;
+		wait for 1 ns;
+		assert (Finished = '1') report "Incorrect output #5: expected 1, received " & std_logic'image(Finished) severity error;
+		-- ^^ Verifico che segnali di start sfasati non diano problemi
 		
 		
-
-      -- insert stimulus here 
+		wait for 20 ns;
+		reset <= '1';
+		wait for clock_period * 5;
+		reset <= '0';
+		wait for clock_period * 1;
+		
+		start <= '1';
+		wait for clock_period * 9 / 10;
+		start <= '0';
+		wait for clock_period / 10;
+		wait for clock_period * 10; -- 10 colpi di clock dall'alzarsi di start
+		assert (Finished = '1') report "Incorrect output #6: expected 1, received " & std_logic'image(Finished) severity error;
+		-- ^^ Verifico che segnali di start corti non diano problemi
+		-- NOTA: questo test non passerà (in simulazione Behavioral). È un limite progettuale che Start debba durare ALMENO un colpo di clock.
+		-- (questo supponendo il caso peggiore in cui si alzi immediatamente dopo il rising_edge del clock).
+		
+		
+		-- NOTA: Gli assert usati perdono valore in caso di simulazione Post-Route. Questo è dovuto alla (imprevedibile) differenza tra l'istante
+		-- in cui viene effettuato l'assert e quello in cui effettivamente cambia il valore (differenza dovuta ai ritardi della simulazione).
+		-- Un controllo manuale è più appropriato.
+		-- L'ultimo controllo manuale post-route è stato effettuato il 31/12 e ha confermato la robustezza del componente. È stato inoltre
+		-- rilevato un ritardo tra istante teorico e istante pratico di circa 7.2 ns.
 
       wait;
    end process;
