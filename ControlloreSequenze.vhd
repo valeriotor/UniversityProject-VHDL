a738d6164e9b3055595d9b6572af9f9737f43972
Valerio Torsiello <valeriotorsiello@libero.it>
2020-01-22 22:21:38 +0100

    Cambiato ControlloreSequenze per essere più conforme allo standard Mealy
    Precedentemente il processo Mealy aveva solo Clock nella sua sensitivity list, e i cambiamenti venivano presi in considerazione solo all'alzarsi del clock.
    
    Adesso ogni cambiamento di input avvia il processo, che è stato modificato in modo da tener conto del maggior numero di combinazioni.
    
    Un altro risultato di ciò è che il processo di StateChange adesso ha luogo al rising edge del clock, e non più al falling edge.

--- ControlloreSequenze.vhd
+++ ControlloreSequenze.vhd
@@ -48,15 +48,17 @@ signal counter : integer range 0 to 4 := 0;
  
 begin
 
-Mealy : process (Clock)
+Mealy : process (curr_state, counter, EnterCode, EnableKey, Reset, Code, Stored_Key)
 begin
-	if(rising_edge(Clock)) then
-		if(Reset = '1' OR EnableKey = '0') then
-			next_state <= WAITING;
-			Success <= '0';
-			--report "Stored Key RES/ENABLE: " & std_logic'image(stored_key(3)) & std_logic'image(stored_key(2)) & std_logic'image(stored_key(1)) & std_logic'image(stored_key(0));			
-		else
-			--report "Stored Key ELSE: " & std_logic'image(stored_key(3)) & std_logic'image(stored_key(2)) & std_logic'image(stored_key(1)) & std_logic'image(stored_key(0));			
+	--if(rising_edge(Clock)) then
+	Success <= '0';
+	next_state <= WAITING;
+		if(Reset = '0' AND EnableKey = '1') then
+--			next_state <= WAITING;
+--			Success <= '0';
+--			--report "Stored Key RES/ENABLE: " & std_logic'image(stored_key(3)) & std_logic'image(stored_key(2)) & std_logic'image(stored_key(1)) & std_logic'image(stored_key(0));			
+--		else
+			--report "Stored Key: " & std_logic'image(stored_key(3)) & std_logic'image(stored_key(2)) & std_logic'image(stored_key(1)) & std_logic'image(stored_key(0));			
 		
 			-- TRANSIZIONI MEALY
 			case curr_state is
@@ -67,6 +69,8 @@ begin
 					--report "Timer Start/Timer End: " & std_logic'image(timer_start) & "/" & std_logic'image(timer_end);
 					if(counter = 4) then
 						next_state <= STEADY;
+					else
+						next_state <= READY;
 					end if;
 				when STEADY =>
 					if(EnterCode = '1') then
@@ -75,18 +79,24 @@ begin
 							Success <= '1';
 						else
 							next_state <= NO_MATCH;
-						end if;	
+						end if;
+					else
+						next_state <= STEADY;
 					end if;
-				when others => 
+				when MATCH =>
+					next_state <= MATCH;
+					Success <= '1';
+				when NO_MATCH =>
+					next_state <= NO_MATCH;
 			end case;
 			-- FINE TRANSIZIONI MEALY
 		end if;
-	end if;	
+	--end if;	
 end process;
 
 StateChange : process (Clock)
 begin
-	if(falling_edge(Clock)) then
+	if(rising_edge(Clock)) then
 		curr_state <= next_state;
 	end if;
 end process;
